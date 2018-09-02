4d9d4458738562cee0abdbeec6f5ffed59bc1955
Taylor Hammons <taylor@thinkful.com>
2018-09-02 16:37:28 -0500

    finished integration tests challenge

---
+++ test/test-recipes.js
@@ -0,0 +1,87 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, closeServer, runServer} = require('../server');
+
+const expect = chai.expect;
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Recipes', function() {
+
+    before(function() {
+        return runServer();
+    });
+
+    after(function() {
+        return closeServer();
+    });
+
+    it('should list items on GET', function() {
+        return chai.request(app)
+            .get('/recipes')
+            .then(function(res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('array');
+
+                const expectedKeys = ['name', 'ingredients'];
+                res.body.forEach(function(item){
+                    expect(item).to.be.a('object');
+                    expect(item).to.include.keys(expectedKeys);
+                });
+            });
+    });
+
+    it('should add an item on POST', function() {
+        const newItem = {name: 'coffee', ingredients: ['coffee beans']};
+        return chai.request(app)
+            .post('/recipes') 
+            .send(newItem)
+            .then(function(res){
+                expect(res).to.have.status(201);
+                expect(res).to.be.json;
+                expect(res.body).to.include.keys('id', 'name', 'ingredients');
+                expect(res.body.id).to.not.equal(null);
+
+                expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id}));
+            });
+
+    });
+
+    it('should update items on PUT', function() {
+        const updateData = {
+            name: 'penis',
+            ingredients: ['XY Chromosomes']
+        };
+        return chai.request(app)
+            .get('/recipes')
+            .then(function(res){
+                updateData.id = res.body[0].id;
+                return chai.request(app)
+                    .put(`/recipes/${updateData.id}`)
+                    .send(updateData)
+            })
+            .then(function(res){
+                res.should.have.status(204);
+            });
+    });
+
+
+    it('should delete item on DELETE', function() {
+        return chai.request(app)
+            .get('/recipes')
+            .then(function(res){
+                return chai.request(app)
+                    .delete(`/recipes/${res.body[0].id}`);
+            })
+            .then(function(res){
+                expect(res).to.have.status(204);
+            });
+    });
+
+
+
+});
